39e3a72ab43e552db60461baa13e0b4d19bfb004
KiDj00 <aleksa.djordjevic2312@gmail.com>
2022-11-17 03:08:11 +0100

    Izmena za primarne kljuceve i komentare u bazi

--- minerals.sql
+++ minerals.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2022 at 10:46 PM
+-- Generation Time: Nov 17, 2022 at 03:04 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -39,14 +39,11 @@ CREATE TABLE `comment` (
 --
 
 INSERT INTO `comment` (`id`, `username`, `content`, `mineral_id`) VALUES
-(1, 'Aleksa Djordjevic', 'Odlican odnos cene i kvaliteta', 2),
-(2, 'Pera Peric', 'Sve pohvale', 3),
-(3, 'Aleksa Aleksic', 'Odlican Malachite', 1),
-(4, 'Pera Peric', 'Kliko je tezak?', 13),
-(6, 'Pera Peric', 'Odlicna stvar za kolekciju', 2),
-(7, 'Aleksa Djordjevic', 'Bas lep kamen', 19),
-(8, 'Pera Peric', 'Da li je cena fiksna?', 17),
-(9, 'Aleksa Djordjevic', 'Nadam se da cu nekada imati ovaj divni kamen u svojoj kolekciji', 17);
+(1, 'Aleksa Djordjevic', 'Odlican odnos cene i kvaliteta', 1),
+(2, 'Pera Peric', 'Sve pohvale', 2),
+(3, 'Pera Peric', 'Odlicna stvar za kolekciju', 1),
+(4, 'Pera Peric', 'Da li je cena fiksna?', 4),
+(5, 'Aleksa Djordjevic', 'Nadam se da cu nekada imati ovaj divni kamen u svojoj kolekciji', 4);
 
 -- --------------------------------------------------------
 
@@ -68,18 +65,18 @@ CREATE TABLE `mineral` (
 --
 
 INSERT INTO `mineral` (`id`, `username`, `title`, `locality`, `price`, `img`) VALUES
-(2, 'aleksa', 'Baryte', 'Turt mine, Romania', 120, 'https://www.turkishminerals.org/wp-content/uploads/2020/04/NATURAL-STEATITE-TALC.png'),
-(3, 'aleksa', 'Boron', 'Itauz mine, Kazahstan', 90, 'https://scdn.onnit.com/images/product-page/key-minerals/boron.png'),
-(15, 'aleksa', 'Quartz Crystal Cluster', 'Madagascar', 670, 'https://www.kristalgrad.co.rs/slike/o-nama.png'),
-(17, 'aleksa', 'Sodalite', 'Langesundsfjord, Norway', 3500, 'https://png2.cleanpng.com/sh/d09c950bbf12ebf833f26bc5086ccfe4/L0KzQYm3VcI2N6R4iZH0aYP2gLBuTft6aZ9ujNc2Y4L8g8XojL1pbZJxgdDwLXfofcT7jB5mNZ5uhtd7YXywRbO3WMg0apRmTNdqZEmxSIq5VcQ3QGk2TaQCMki3RoeBVsc2PV91htk=/kisspng-kyanite-crystal-healing-gemstone-mineral-5b0883bca4ead9.8925468815272846686755.png'),
-(20, 'aleksa', 'Sapphire ', 'Port Orford, Canada', 4000, 'https://www.gia.edu/images/70094.png'),
-(23, 'aleksa', 'Fluorite  ', 'Larkin`s Quarry, Ireland', 780, 'https://www.spiriferminerals.com/produkty/23412/bem34a.jpg'),
-(24, 'aleksa', 'Dark Purple Amethyst Cluster', 'Minas Gerais, Brazil', 2300, 'https://assets3.fossilera.com/sp/565035/amethyst/708x500%3E/quartz-var-amethyst-goethite.jpg'),
-(25, 'aleksa', 'Green-Black Calcite Crystal Cluster', 'Sweetwater Mine, Missouri', 1990, 'https://assets1.fossilera.com/sp/482676/calcite/708x500%3E/calcite-chalcopyrite.jpg'),
-(26, 'aleksa', 'Veszelyite', 'Palabanda, Republic of the Congo', 2800, 'https://www.spiriferminerals.com/produkty/23057/beh17a.jpg'),
-(27, 'aleksa', 'Bicolor Elbaite Tourmaline Crystal', 'Cruzeiro Mine, Brazil', 1295, 'https://assets1.fossilera.com/sp/578162/rubellite/708x500%3E/tourmaline-var-elbaite.jpg'),
-(28, 'aleksa', 'Vibrant Azurite and Malachite Crystal Association ', 'Liufengshan Mine, China', 595, 'https://assets3.fossilera.com/sp/587724/azurite/708x500%3E/azurite-malachite.jpg'),
-(29, 'aleksa', 'Beam Calcite Crystal Cluster with Phantoms', 'Atlas Mountains, Morocco', 402, 'https://assets1.fossilera.com/sp/545008/calcite/708x500%3E/calcite.jpg');
+(1, 'aleksa', 'Baryte', 'Turt mine, Romania', 120, 'https://www.turkishminerals.org/wp-content/uploads/2020/04/NATURAL-STEATITE-TALC.png'),
+(2, 'aleksa', 'Boron', 'Itauz mine, Kazahstan', 90, 'https://scdn.onnit.com/images/product-page/key-minerals/boron.png'),
+(3, 'aleksa', 'Quartz Crystal Cluster', 'Madagascar', 670, 'https://www.kristalgrad.co.rs/slike/o-nama.png'),
+(4, 'aleksa', 'Sodalite', 'Langesundsfjord, Norway', 3500, 'https://png2.cleanpng.com/sh/d09c950bbf12ebf833f26bc5086ccfe4/L0KzQYm3VcI2N6R4iZH0aYP2gLBuTft6aZ9ujNc2Y4L8g8XojL1pbZJxgdDwLXfofcT7jB5mNZ5uhtd7YXywRbO3WMg0apRmTNdqZEmxSIq5VcQ3QGk2TaQCMki3RoeBVsc2PV91htk=/kisspng-kyanite-crystal-healing-gemstone-mineral-5b0883bca4ead9.8925468815272846686755.png'),
+(5, 'aleksa', 'Sapphire ', 'Port Orford, Canada', 4000, 'https://www.gia.edu/images/70094.png'),
+(6, 'aleksa', 'Fluorite  ', 'Larkin`s Quarry, Ireland', 780, 'https://www.spiriferminerals.com/produkty/23412/bem34a.jpg'),
+(7, 'aleksa', 'Dark Purple Amethyst Cluster', 'Minas Gerais, Brazil', 2300, 'https://assets3.fossilera.com/sp/565035/amethyst/708x500%3E/quartz-var-amethyst-goethite.jpg'),
+(8, 'aleksa', 'Green-Black Calcite Crystal Cluster', 'Sweetwater Mine, Missouri', 1990, 'https://assets1.fossilera.com/sp/482676/calcite/708x500%3E/calcite-chalcopyrite.jpg'),
+(9, 'aleksa', 'Veszelyite', 'Palabanda, Republic of the Congo', 2800, 'https://www.spiriferminerals.com/produkty/23057/beh17a.jpg'),
+(10, 'aleksa', 'Bicolor Elbaite Tourmaline Crystal', 'Cruzeiro Mine, Brazil', 1295, 'https://assets1.fossilera.com/sp/578162/rubellite/708x500%3E/tourmaline-var-elbaite.jpg'),
+(11, 'aleksa', 'Vibrant Azurite and Malachite Crystal Association ', 'Liufengshan Mine, China', 595, 'https://assets3.fossilera.com/sp/587724/azurite/708x500%3E/azurite-malachite.jpg'),
+(12, 'aleksa', 'Beam Calcite Crystal Cluster with Phantoms', 'Atlas Mountains, Morocco', 402, 'https://assets1.fossilera.com/sp/545008/calcite/708x500%3E/calcite.jpg');
 
 --
 -- Indexes for dumped tables
@@ -111,7 +108,7 @@ ALTER TABLE `comment`
 -- AUTO_INCREMENT for table `mineral`
 --
 ALTER TABLE `mineral`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
